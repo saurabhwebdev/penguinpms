0feb7da6da2b3a34394131c6279ae370cdbfae02
saurabhwebdev <72278682+saurabhwebdev@users.noreply.github.com>
2024-08-18 03:44:30 +0530

    Logo corrected it was not showing

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ function showLoginForm() {
     <div class="flex items-center justify-center min-h-screen bg-gray-100">
         <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
             <div class="text-center mb-4">
-                <img src="./img/logo.png" alt="Penguin Icon" class="w-16 h-16 mx-auto">
+                <img src="logo.png" alt="Penguin Icon" class="w-16 h-16 mx-auto">
                 <h3 class="text-xl font-bold">June-PMS</h3>
             </div>
             <h3 class="text-2xl font-bold text-center">Login to your account</h3>
